236faf2261fbc2262ddb92f7356e80f758d53f37
romanunr02 <romanunr02@gmail.com>
2025-04-25 23:19:54 +0300

    prepare test files

--- cypress/e2e/example.spec.ts
+++ cypress/e2e/example.spec.ts
@@ -1,6 +1,17 @@
+import { TEMP_ENVIRONMENT_URL } from "../support/constants";
 
 describe("Test", () => {
+
+    before(() => {
+        cy.interceptConsoleErrors()
+        cy.visit(TEMP_ENVIRONMENT_URL)
+    })
+
     it("It works", () => {
 
+    })
+
+    it("Verify there are no console errors", () => {
+        cy.assertNoConsoleErrors()
     })
 })
